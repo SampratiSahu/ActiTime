d04e61d8d6000e2b1b2e3c16de3921446e0ff127
SampratiSahu <sampratisahu@gmail.com>
2023-04-03 11:50:45 +0530

    new file LastDayOfSelenium.java file added

---
+++ src/test/java/com/actiTIME/testcase/LastClassOfSelenium.java
@@ -0,0 +1,12 @@
+package com.actiTIME.testcase;
+
+import org.testng.annotations.Test;
+
+public class LastClassOfSelenium 
+{
+	@Test
+	public void lastday()
+	{
+		System.out.println("enjoy !!!!!!");
+	}
+}
